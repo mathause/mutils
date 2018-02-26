f30547747b7c2381171647a039140f00ef63ce87
Mathias Hauser <mathias.hauser@env.ethz.ch>
2018-02-26 11:27:20 +0100

    geo: CosWgt

---
+++ mutils/geo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Author: Mathias Hauser
+#Date: 
+
+
+import numpy as np
+
+
+def CosWgt(lat):
+    """cosine-weighted latitude"""
+    return np.cos(np.deg2rad(lat))
+
+
+
+
+
+
